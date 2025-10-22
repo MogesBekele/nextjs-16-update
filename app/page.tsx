0f4a7ce40da10793a65cd958be4c239998bac93d
MogesBekele <mogesbekele32@gmail.com>
2025-10-22 10:59:28 +0300

    layout diplatation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 export default function HomePage(){
   return(
 
-    <div className="flex items-center justify-center my-auto text-blue-600">
+    <div className="flex flex-col items-center justify-center my-auto text-blue-600">
       <h1>
         new next js 16 update
       </h1>
+    <div>
+
+      <div className="flex items-center justify-center my-auto text-blue-600"></div>
+        <h2>
+          with tailwind css
+        </h2>
+    </div>
+
+    
+
+
     </div>
   )
 }
