72b725880313129acaaeeb11b030ebaa0921d90d
MogesBekele <mogesbekele32@gmail.com>
2025-10-25 10:20:49 +0300

    use data-access layer

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,15 @@
 import { NextResponse } from "next/server";
 
+export function proxy(req: Request) {
+  //autentecation for the api
+  // jwt is not recommeneded
+  // autenticate with kinde third party service
+  // use kinde for autentication and authorization
+  //
 
-
-export function proxy(req: Request){
-  return withAuth(req)
-  
-//autentecation for the api
-// jwt is not recommeneded
-// autenticate with kinde third party service 
-// use kinde for autentication and authorization
-
-
-  
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/others"]
-}
+  matcher: ["/", "/others"],
+};
