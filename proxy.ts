5f3662bfc01720f4fd62420d08d0a6309dc0541f
MogesBekele <mogesbekele32@gmail.com>
2025-10-25 10:24:27 +0300

    also we can use coodies and header

--- proxy.ts
+++ proxy.ts
@@ -6,8 +6,9 @@ export function proxy(req: Request) {
   // autenticate with kinde third party service
   // use kinde for autentication and authorization
   // data access layer if the default
+  //  also use coodies and header
 
-  
+
   return NextResponse.next();
 }
 
