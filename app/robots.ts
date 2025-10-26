f11a9099d7267965ca22be744466032fc05d7c08
MogesBekele <mogesbekele32@gmail.com>
2025-10-26 11:42:56 +0300

    create the robots for metadata

---
+++ app/robots.ts
@@ -0,0 +1,22 @@
+//for privacy police page
+
+import { MetadataRoute } from "next";
+
+//block those sencetive areas
+export default function robots():MetadataRoute.Robots{
+  return{
+    rules:[{
+      userAgent:"*",
+      allow: "/",
+      disallow:"/sencetive-data",
+
+    },
+    {
+      userAgent:"googlebot",
+      allow:"/",
+      disallow:"/sensetive-data"
+
+    }
+  ]
+  }
+}
