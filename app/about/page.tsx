48b14b3caeaf1c41d04d4dce7b81d6aec7ff39cd
MogesBekele <mogesbekele32@gmail.com>
2025-10-26 11:16:23 +0300

    add metadata in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,14 @@
 
-
+export const metadata = {
+  title: "About page",
+  description: "this is about page description for seo"
+}
 export default function AboutPage(){
   return (
 
     <div>
       <h1>this is about page on client side</h1>
-      
+
     </div>
   )
 }
