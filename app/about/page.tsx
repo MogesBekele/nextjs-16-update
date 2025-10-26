351f066345611f742c4efa3edadc563aff9eb182
MogesBekele <mogesbekele32@gmail.com>
2025-10-26 11:19:37 +0300

    add opengraph on metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,24 @@
-
 export const metadata = {
   title: "About page",
   description: "this is about page description for seo",
-  keywords: "different keywork to describe the page"
-}
-export default function AboutPage(){
+  keywords: "different keywork to describe the page",
+  openGraph: {
+    title: "About page",
+    description: "this is about page description for seo",
+    url: "https://www.example.com",
+    siteName: "My site",
+    images: [
+      {
+        url: "https://www.example.com/og-image.png",
+        width: 800,
+        height: 600,
+      },
+  ]}
+};
+export default function AboutPage() {
   return (
-
     <div>
       <h1>this is about page on client side</h1>
-
     </div>
-  )
+  );
 }
