4c699a5ffcae47e67c8433025e470e9a72942c0f
MogesBekele <mogesbekele32@gmail.com>
2025-10-27 11:16:59 +0300

    use cache for store data

--- app/frontendDesignPattern/page.ts
+++ app/frontendDesignPattern/page.ts
@@ -1,2 +1,7 @@
 
+// the following are some notes on design patterns in frontend development
+// dont jump to framework before mastring the fundamentals of design patterns
+
+// http caching design pattern memorization, store copy of data temporarily
+// caching means storing copies of data (like files, images, or API responses) temporarily so that it can be quickly retrieved the next time it’s needed — without having to fetch or compute it again.
 
