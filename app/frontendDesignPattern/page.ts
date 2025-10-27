87220f6a4e269fe03a9dc8ba63eea1ab33549d2a
MogesBekele <mogesbekele32@gmail.com>
2025-10-27 11:19:24 +0300

    improve lazy loading

--- app/frontendDesignPattern/page.ts
+++ app/frontendDesignPattern/page.ts
@@ -5,3 +5,6 @@
 // http caching design pattern memorization, store copy of data temporarily
 // caching means storing copies of data (like files, images, or API responses) temporarily so that it can be quickly retrieved the next time it’s needed — without having to fetch or compute it again.
 
+//content negotiation with the client
+//content negotiation is a mechanism that makes it possible to serve different versions of a document (or more generally, a resource) at the same URI, so that user agents can specify which version fits their capabilities the best.
+
